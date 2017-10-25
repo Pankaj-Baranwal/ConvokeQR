70664f1c86b4a5ec1d79ff40a0a8a638d3a59c96
Pankaj Baranwal <pankajisadeveloper@gmail.com>
2017-10-25 06:15:18 +0530

    Working QR Scanner fragment

--- app/src/main/java/com/example/pankaj/convokeqr/fragment/QRScanner.java
+++ app/src/main/java/com/example/pankaj/convokeqr/fragment/QRScanner.java
@@ -3,40 +3,40 @@ package com.example.pankaj.convokeqr.fragment;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
+import android.util.SparseArray;
 import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.widget.TextView;
+
 import com.example.pankaj.convokeqr.R;
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
 
-import github.nisrulz.qreader.QRDataListener;
-import github.nisrulz.qreader.QREader;
+import java.io.IOException;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link QRScanner.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link QRScanner#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class QRScanner extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String ARG_PARAM1 = "which_qr";
 
     // TODO: Rename and change types of parameters
     private String mParam1;
-    private String mParam2;
+    SurfaceView cameraView;
+    TextView barcodeInfo;
+    BarcodeDetector barcodeDetector;
+    CameraSource cameraSource;
+    String vehicle_id;
 
-    private OnFragmentInteractionListener mListener;
-
-    private SurfaceView mySurfaceView;
-    private QREader qrEader;
+    private OnQRFragmentInteractionListener mListener;
 
     public QRScanner() {
         // Required empty public constructor
@@ -47,15 +47,13 @@ public class QRScanner extends Fragment {
      * this fragment using the provided parameters.
      *
      * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment QRScanner.
      */
     // TODO: Rename and change types and number of parameters
-    public static QRScanner newInstance(String param1, String param2) {
+    public static QRScanner newInstance(String param1) {
         QRScanner fragment = new QRScanner();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -65,40 +63,80 @@ public class QRScanner extends Fragment {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_qrscanner, container, false);
-        mySurfaceView = view.findViewById(R.id.camera_view);
-        qrEader = new QREader.Builder(getContext(), mySurfaceView, new QRDataListener() {
-            @Override
-            public void onDetected(final String data) {
-                Log.e("QREader", "Value : " + data);
-            }
-        }).facing(QREader.BACK_CAM)
-                .enableAutofocus(true)
-                .height(mySurfaceView.getHeight())
-                .width(mySurfaceView.getWidth())
-                .build();
-        return view;
+        return inflater.inflate(R.layout.fragment_qrscanner, container, false);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        cameraView = view.findViewById(R.id.camera_view);
+        barcodeInfo = view.findViewById(R.id.qr_info);
+        barcodeDetector =
+                new BarcodeDetector.Builder(getContext())
+                        .setBarcodeFormats(Barcode.QR_CODE)
+                        .build();
+
+        cameraSource = new CameraSource
+                .Builder(getContext(), barcodeDetector)
+                .setRequestedPreviewSize(10, 10).setAutoFocusEnabled(true)
+                .build();
+        cameraView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            public void surfaceCreated(SurfaceHolder holder) {
+                try {
+                    cameraSource.start(cameraView.getHolder());
+                } catch (IOException ie) {
+                    Log.e("CAMERA SOURCE", ie.getMessage());
+                } catch (SecurityException sc) {
+                    Log.e("error", "security");
+                }
+            }
+
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+            }
+
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                cameraSource.stop();
+            }
+        });
+
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+
+            public void release() {
+
+            }
+
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                final SparseArray<Barcode> barcodes = detections.getDetectedItems();
+
+                if (barcodes.size() != 0) {
+                    barcodeInfo.post(new Runnable() {    // Use the post method of the TextView
+                        public void run() {
+                            if (mParam1.contentEquals("CheckIn")){
+                                mListener.onQRFragmentInteraction(barcodes.valueAt(0).displayValue);
+                            }else if (mParam1.contentEquals("150")){
+                                barcodeInfo.setText(barcodes.valueAt(0).displayValue);
+                            }else if (mParam1.contentEquals("300")){
+                                barcodeInfo.setText(barcodes.valueAt(0).displayValue);
+                            }
+                        }
+                    });
+                }
+            }
+        });
     }
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
+        if (context instanceof OnQRFragmentInteractionListener) {
+            mListener = (OnQRFragmentInteractionListener) context;
         } else {
             throw new RuntimeException(context.toString()
                     + " must implement OnFragmentInteractionListener");
@@ -111,18 +149,8 @@ public class QRScanner extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
+    public interface OnQRFragmentInteractionListener {
         // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+        void onQRFragmentInteraction(String string);
     }
 }
